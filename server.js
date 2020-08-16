9bb37f4e3cc280f7bf0bbc27b5263723d89af98a
Josemilson Júnior <josemilson.junior@a.ficr.edu.br>
2020-08-16 13:54:45 -0300

    CONF. BÁSICA COM ROTAS

--- server.js
+++ server.js
@@ -1,8 +1,35 @@
-//IMPORTA MODULO EXPRESS
+//IMPORTAR MODULO EXPRESS E BODYPARSER
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
 
+//INSTANCIA O EXPRESS E DEFINE A PORTA
+const app = express();
 const port = process.env.PORT || 3000;
 
+//DEFINIR O BODYPARSER
+//(FILTRA A INF. FORMULÁRIOS DO BODY)
+app.use(bodyParser.urlencoded({extended: true}));
+//(FILTRA A INF. JSON DO BODY)
+app.use(bodyParser.json());
+
+//DEFININDO ROTAS DE REQUISIÇÃO RAIZ
+app.route('/')
+    .get((req, res) => {
+        res.send('Respondendo no método GET Raiz');
+    })
+    .post((req, res) => {
+        res.send('Respondendo no método POST Raiz');
+    });
+
+//DEFININDO ROTAS DE REQUISIÇÃO PARA USUÁRIOS
+app.route('/usuarios')
+    .get((req, res) => {
+        res.send('Respondendo no método GET em Usuários');
+    })
+    .post((req, res) => {
+        res.send('Respondendo no método POST em Usuários');
+    });
+
+//DEFININDO PORTA QUE O SERVIDOR VAI OUVIR
 app.listen(port);
-console.log('Servidor Funcionando, na porta: ', port)
+console.log('Servidor Funcionando, na porta: ', port);
